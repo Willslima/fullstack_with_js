c62439be7dca3a695e2ad8e7148c4fba518c3dd1
Willian <williansantos38@gmail.com>
2021-09-08 16:07:54 -0300

    finalizado a parte de logica da calculadora, projeto esta funcionando

--- calculadora/index.js
+++ calculadora/index.js
@@ -4,31 +4,18 @@ function limparDisplay() {
 
 function btnNumeros(index) {
     let key = document.querySelectorAll('.botao')[index].innerText
-
-
-        document.getElementById('tela').value = document.getElementById('tela').value + key
+    document.getElementById('tela').value = document.getElementById('tela').value + key
+    let display = document.getElementById('tela').value
     
 }
 
-function somar() {
-    limparDisplay()
-}
-
-function subtrair() {
-    limparDisplay()
-}
-
-function multiplicar() {
-    limparDisplay()
-}
-
-function dividir() {
-    limparDisplay()
-}
-
 function resultado() {
-    limparDisplay()
-}
+    let display = document.getElementById('tela').value
+    let result = eval(display)
+    document.getElementById('tela').value = result
+    console.log(result)
+} 
+
  function ponto(index){
     let key = document.querySelectorAll('.botao')[index].innerText
 
